0e34b8a79c2a3bf0484314779d398b8d6e50d905
missmiss5 <48143963+missmiss5@users.noreply.github.com>
2021-05-20 23:53:41 +0700

    Add files via upload

---
+++ MiningTimeOut.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         MiningTimeOut
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       turin#8657 discord
+// @match        http://localhost:6336/
+// @icon         https://img-premium.flaticon.com/png/512/1839/1839000.png?token=exp=1621404256~hmac=cb453e629dc4316af7103ea360ade6c1
+// @grant        none
+// ==/UserScript==knklj
+
+(function () {
+    var WaitForMining = setInterval(() => {
+        var tags_i = document.getElementsByTagName("button");
+        for (var i = 0; i < tags_i.length; i++) {
+            if(tags_i[i].innerHTML.indexOf("Mining") !== -1)
+        	{
+                console.warn("Mining is found....!");
+        		setTimeout(() => {
+                    var tags_j = document.getElementsByTagName("button");
+                    for (var j = 0; j < tags_j.length; j++) {
+                        if(tags_j[j].innerHTML.indexOf("Mining") !== -1) {
+                            location.reload();
+                        }
+                    }
+                },300000)
+        	}
+        }
+    }, 5000);
+})();
