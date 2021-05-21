02b41eedc40aab0e23dbb46c855d1976d29debdd
missmiss5 <48143963+missmiss5@users.noreply.github.com>
2021-05-21 09:16:44 +0700

    Update MiningTimeOut.user.js

--- MiningTimeOut.user.js
+++ MiningTimeOut.user.js
@@ -4,7 +4,7 @@
 // @homepage     https://github.com/missmiss5/AWScript
 // @downloadURL  https://github.com/missmiss5/AWScript/raw/main/MiningTimeOut.user.js
 // @updateURL    https://github.com/missmiss5/AWScript/raw/main/MiningTimeOut.user.js
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       turin#8657
 // @match        http://localhost:6336/
