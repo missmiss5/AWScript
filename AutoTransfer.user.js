29ebea419addeca4e3da75b70f6ce54f9329d409
missmiss5 <48143963+missmiss5@users.noreply.github.com>
2021-05-21 12:37:03 +0700

    Create AutoTransfer.user.js

---
+++ AutoTransfer.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         AutoTransfer
+// @namespace    https://gist.github.com/missmiss5
+// @homepage     https://github.com/missmiss5/AWScript
+// @downloadURL  https://github.com/missmiss5/AWScript/raw/main/AutoSwap.user.js
+// @updateURL    https://github.com/missmiss5/AWScript/raw/main/AutoSwap.user.js
+// @version      0.0
+// @description  try to take over the world!
+// @author       turin#8657
+// @match        http://localhost:6336/
+// @icon        
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
