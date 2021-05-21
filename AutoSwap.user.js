60f3370a229720698581779577e739fd22349b5c
missmiss5 <48143963+missmiss5@users.noreply.github.com>
2021-05-21 12:36:20 +0700

    Update AutoSwap.user.js

--- AutoSwap.user.js
+++ AutoSwap.user.js
@@ -2,9 +2,9 @@
 // @name         AutoSwap
 // @namespace    https://gist.github.com/missmiss5
 // @homepage     https://github.com/missmiss5/AWScript
-// @downloadURL  https://github.com/missmiss5/AWScript/raw/main/MiningTimeOut.user.js
-// @updateURL    https://github.com/missmiss5/AWScript/raw/main/MiningTimeOut.user.js
-// @version      0.3
+// @downloadURL  https://github.com/missmiss5/AWScript/raw/main/AutoSwap.user.js
+// @updateURL    https://github.com/missmiss5/AWScript/raw/main/AutoSwap.user.js
+// @version      0.0
 // @description  try to take over the world!
 // @author       turin#8657
 // @match        http://localhost:6336/
